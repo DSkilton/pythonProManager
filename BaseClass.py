ffcb5f1882a262e579f78334cbef9c9c19d5ad4c
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2021-11-29 15:08:04 +0000

    updated BaseClass module and Website module

--- BaseClass.py
+++ BaseClass.py
@@ -1,7 +1,6 @@
-class baseClass:
+class BaseClass:
     def __init__(self, project_name, deadline, client, price):
         self.project_name = project_name
         self.deadline = deadline
         self.client = client
         self.price = price
-
---
+++ website.py
@@ -0,0 +1,6 @@
+from BaseClass import BaseClass
+
+
+class website(BaseClass):
+    def __init__(self, server_name, num_of_pages, project_name, deadline, client, price):
+        super(website, self, project_name, deadline, client, price)
