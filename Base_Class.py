28c3e4995b79508a29e96cf30eb06caaa00b9a3f
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2021-11-29 13:50:30 +0000

    initial commit

---
+++ Base_Class.py
@@ -0,0 +1,7 @@
+class baseClass:
+    def __init__(self, project_name, deadline, client, price):
+        self.project_name = project_name
+        self.deadline = deadline
+        self.client = client
+        self.price = price
+
