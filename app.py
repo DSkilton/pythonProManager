13c1bf034abf37aceff59e1c53367019b6a81e2a
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2021-12-04 10:54:29 +0000

    added game and app class

---
+++ app.py
@@ -0,0 +1,8 @@
+from BaseClass import BaseClass
+
+
+class app(BaseClass):
+    def __init__(self, server_name, num_of_pages, project_name, deadline, client, price):
+        super(app, self, project_name, deadline, client, price)
+
+
---
+++ game.py
@@ -0,0 +1,8 @@
+from BaseClass import BaseClass
+
+
+class game(BaseClass):
+    def __init__(self, server_name, num_of_pages, project_name, deadline, client, price):
+        super(game, self, project_name, deadline, client, price)
+
+
