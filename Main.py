f5b40861c7b36073fa36cc5cb6d5e5fabe2907e8
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2021-12-04 12:20:38 +0000

    added menu function to Main module

--- Main.py
+++ Main.py
@@ -4,12 +4,22 @@ from app import app
 from game import game
 
 
+def menu():
+    print("Enter a number to follow the option\n"
+          "1. New website\t 2. Second Opt\n"
+          "3. New game \t 4. Fourth Opt\n"
+          "5. New app \t\t 6. sixth Opt\n"
+          "7. Seventh Opt \t 8. Exit")
+
+
+menu()
+
+
 def test():
     name = website()
     name.project_name = "Project"
     name.deadline = "deadline"
     name.server_name = "192.172.0.0"
+    name.get_base()
     print(name.project_name , name.server_name)
 
-
-test()
--- website.py
+++ website.py
@@ -6,6 +6,5 @@ class website(BaseClass):
     number_of_pages = ""
 
     def set_web_details(self, server_name, num_of_pages):
-
         self.server_name = server_name
         self.number_of_pages = num_of_pages
